873f9753fb4c0841615a3920def640a5bb2baf03
eshwar <ramanieshwar091@gmail.com>
2021-05-10 19:55:21 +0530

    version-12

--- components/users.sh
+++ components/users.sh
@@ -5,19 +5,18 @@
 
 apt update
 #Head "install java version java-8"
-apt install openjdk-8-jdk -y &>>$LOG
+apt install openjdk-8-jdk -y
 #Check $?
 
 #Head "Install maven"
-apt install maven -y &>>$LOG
+apt install maven -y
 #Check $?
 
 #Head "fetch git code"
-git clone https://github.com/zelar-soft-todoapp/users.git &>>$LOG
-
+git clone https://github.com/zelar-soft-todoapp/users.git
 
 #Head "entering into the directory"
-cd users && mvn clean package &>>$LOG
+cd users && mvn clean package
 #Check $?
 
 #Head "entering into the directory"
@@ -26,9 +25,9 @@ cd target
 
 
 #Head "moving the directory"
-mv users-api-0.0.1.jar users.jar &>>$LOG
+mv users-api-0.0.1.jar users.jar
 #Check $?
 
 #Head "setup the service with systemctl"
-mv /home/ubuntu/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users &>>LOG
+mv /home/ubuntu/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users
 #Check $?
