31e08f3685ba5c88d2bb5752665d5dcea2a56ddc
eshwar <ramanieshwar091@gmail.com>
2021-05-15 22:28:55 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
 source components/common.sh
-
+DN="eshwarzelarsoft.host"
 OS_PREREQ
 
-Head "Installing Nginx and Npm"
-sudo apt install nginx -y &>>$LOG
-sudo apt install npm -y &>>$LOG
+Head "Installing Nginx"
+apt install nginx -y &>>$LOG
+systemctl restart nginx
 Check $?
 
-Head "Downloading frontend COMPONENT"
-cd /var/www/html && git clone https://github.com/Hemadurga123/frontend.git &>>$LOG && cd frontend
-
-Head "Building the Code"
-npm install &>>$LOG && npm run build &>>$LOG
+Head "Install npm"
+apt install npm -y &>>$LOG
 Check $?
 
+DOWNLOAD_COMPONENT
 
-Head "Moving the Config file"
-mv frontend.conf /etc/nginx/sites-enabled/default
-sed -i -e "s/LOGIN_ENDPOINT/login.eshwarzelarsoft.host/" -e "s/TODO_ENDPOINT/todo.eshwarzelarsoft.host/" /etc/nginx/sites-enabled/default
+Head "Unzip Downloaded Archive"
+cd /var/www/html &&rm -rf vue && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
 Check $?
 
-Head "Exporting the  Ip adresses"
-export AUTH_API_ADDRESS=http://login.eshwarzelarsoft.host:8080
-export TODOS_API_ADDRESS=http://todo.eshwarzelarsoft.host:8080
+Head "Update Nginx Configuration"
+sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default
 Check $?
 
-Head "Starting the Npm Service"
+Head "pass the end points in service file"
+cd /var/www/html/vue/frontend
+cd config
+export AUTH_API_ADDRESS=http://login.${DN}:8080
+export TODOS_API_ADDRESS=http://todo.${DN}:8080
+Check $?
+
+Head "update frontend configuration"
+cd /var/www/html/vue/frontend  && sudo npm install --unsafe-perm sass sass-loader node-sass wepy-compiler-sass &>>$LOG && npm run build &>>$LOG
+Check $?
+
+head "Start Npm service"
 npm start
-systemctl restart nginx &>>$LOG && systemctl start nginx &>>$LOG systemctl enable nginx &>>$LOG
-systemctl status nginx
+Check $?
--- components/login.sh
+++ components/login.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 OS_PREREQ
-#DN="zsdevops01.xyz"
+DN="eshwarzelarsoft.host"
 
 Head "Installing Golang"
 apt install golang -y &>>$LOG
@@ -11,13 +11,13 @@ Check $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf login && apt install unzip && && cd /tmp && unzip -o /tmp/login.zip &>>$LOG && mv login-main login && cd /home/ubuntu/login && export GOPATH=/home/ubuntu/go && export GOBIN=$GOPATH/bin && go get &>>$LOG && go build
+cd /home/ubuntu && rm -rf login && apt install unzip && unzip -o /tmp/login.zip &>>$LOG && mv login-main login && cd /home/ubuntu/login && export GOPATH=/home/ubuntu/go && export GOBIN=$GOPATH/bin && go get &>>$LOG && go build
 Check $?
 
-#Head "pass the EndPoints in Service File"
-#sed -i -e "s/user_endpoint/user.${DN}/" /home/ubuntu/login/systemd.service
-#Check $?
+Head "pass the EndPoints in Service File"
+sed -i -e "s/user_endpoint/user.${DN}/" /home/ubuntu/login/systemd.service
+Check $?
 
-#Head "Setup the systemd Service"
-#mv /home/ubuntu/login/systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
-#Check $?
+Head "Setup the systemd Service"
+mv /home/ubuntu/login/systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
+Check $?
