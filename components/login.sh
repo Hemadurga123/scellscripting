73d7cbff27f3b66409e85869111696904c984f1b
eshwar <ramanieshwar091@gmail.com>
2021-05-15 21:27:01 +0530

    new

--- components/login.sh
+++ components/login.sh
@@ -2,26 +2,22 @@
 
 source components/common.sh
 OS_PREREQ
+#DN="zsdevops01.xyz"
 
-Head "Installing Go"
+Head "Installing Golang"
 apt install golang -y &>>$LOG
 Check $?
 
-Head "Downloading login Component"
-cd /root/
-git clone https://github.com/Hemadurga123/login.git &>>$LOG && cd login
-rm -rf /etc/systemd/system/login.service
-mv systemd.service /etc/systemd/system/login.service
-sed -i -e "s/USERS_ENDPOINT/users.eshwarzelarsoft.host/" /etc/systemd/system/login.service
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/ubuntu && rm -rf login && unzip -o /tmp/login.zip &>>$LOG && mv login-main login && cd /home/ubuntu/login && export GOPATH=/home/ubuntu/go && export GOBIN=$GOPATH/bin && go get &>>$LOG && go build
 Check $?
 
+#Head "pass the EndPoints in Service File"
+#sed -i -e "s/user_endpoint/user.${DN}/" /home/ubuntu/login/systemd.service
+#Check $?
 
-
-Head "Building Package"
-go build &>>$LOG
+Head "Setup the systemd Service"
+mv /home/ubuntu/login/systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
 Check $?
-
-Head "Restarting Services"
-./login
-systemctl daemon-reload &>>$LOG && systemctl start login && systemctl enable login &>>$LOG
-systemctl status login
