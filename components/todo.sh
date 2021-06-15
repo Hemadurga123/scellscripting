d789f2f77daae525191c2b6e807872b191ae98b8
Hemadurga123 <82637654+Hemadurga123@users.noreply.github.com>
2021-06-15 11:38:22 +0530

    Update todo.sh

--- components/todo.sh
+++ components/todo.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 OS_PREREQ
-DN="eshwarzelarsoft.host"
+DNS="eshwarzelarsoft.host"
 
 Head "Installing npm"
 apt install npm -y &>>$LOG
@@ -11,15 +11,13 @@ Check $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf todo && apt install -y  unzip && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo  && cd todo && npm install &>>$LOG
-
+cd /home/ubuntu && rm -rf todo && apt install unzip &>>$LOG && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo  && cd todo && npm install &>>$LOG
 Check $?
 
 Head "pass the EndPoints in Service File"
-sed -i -e "s/redis-endpoint/redis.${DN}/" /home/ubuntu/todo/systemd.service
+sed -i -e "s/redis-endpoint/redis.${DNS}/" /home/ubuntu/todo/systemd.service
 Check $?
 
 
-Head "Setuping  the systemd Service"
-mv /home/ubuntu/todo/systemd.service /etc/systemd/system/todo.service && systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
-Check $?
+Head "Setup the systemd Service"
+mv /home/ubuntu/todo/sys
