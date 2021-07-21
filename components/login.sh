2b1be3346df4d0e32552707858120e21ea9738a7
Hemadurga123 <82637654+Hemadurga123@users.noreply.github.com>
2021-07-21 17:16:29 +0530

    Update login.sh

--- components/login.sh
+++ components/login.sh
@@ -17,9 +17,6 @@ Head "Installing go language"
 wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local &>>$LOG
 Stat $?
 
-go version
-~/.profile
-
 Head "Exporting to path"
 export PATH=$PATH:/usr/local/go/bin
 Stat $?
