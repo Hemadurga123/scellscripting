777f5457fc98aafbec4910c653482cd6dd08fcd7
hema <Hemadurga@zelarsoft.com>
2021-07-21 20:30:58 +0530

    update-5

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 LID=lt-05defe0cb371e4725
 LVER=1
 #COMPONENT=$1
@@ -24,15 +23,15 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id  Z06517063D64E51K7A9BK --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id 	Z06517063D64E51K7A9BK --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inv
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
 
 if [ "$1" == "all" ]; then
   for instance in frontend redis users login todo ; do
-    Instance_Create $instance
+    Instance_Create $instance-dev
   done
 else
-  Instance_Create $1
+  Instance_Create $1-dev
 fi
